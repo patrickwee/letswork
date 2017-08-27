7da5a9db17406b50ca5f8a77666ee66fe328a4ca
Patrick Sheldon Wee <pkw228@nyu.edu>
2017-08-27 19:04:55 +0400

    First website

---
+++ contact.php
@@ -0,0 +1,16 @@
+if(isset($_POST['submit'])){
+  $to = "patrick@letswork.io"; // this is your Email address
+  $from = $_POST['email']; // this is the sender's Email address
+  $name = $_POST['name'];
+  $mobile = $_POST['mobile'];
+  $subject = "Form submission";
+  $subject2 = "Copy of your form submission";
+  $message = $name . " " . $mobile . " wrote the following:" . "\n\n" . $_POST['message'];
+  $message2 = "Here is a copy of your message " . $name . "\n\n" . $_POST['message'];
+
+  $headers = "From:" . $from;
+  $headers2 = "From:" . $to;
+  mail($to,$subject,$message,$headers);
+  mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+  echo "Mail Sent. Thank you " . $name . ", we will contact you shortly.";
+}
